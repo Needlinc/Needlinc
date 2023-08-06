480a70c660a8ca64e7626bbe4ed1a9291167054c
Needlinc <needlinc@gmail.com>
2023-08-06 14:30:06 +0100

    Add Needlinc colors

---
+++ lib/needlinc/colors/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NeedlincColors {
+  static const Color blue1 = Color(0xFF176AF1);
+  static const Color blue2 = Color(0xFF4987EC);
+  static const Color blue3 = Color(0xFF8AA6E5);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black1 = Color(0xFF000000);
+  static const Color black2 = Color(0xFF76767A);
+  static const Color black3 = Color(0xFFD8D8DC);
+}
