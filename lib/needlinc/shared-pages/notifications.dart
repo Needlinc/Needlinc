f52183c97eb96c175ba063e51fd5c5e608a2d700
XRichardroidX <oguzieibehrichard@gmail.com>
2023-10-05 15:37:37 +0100

    Updating the notification page

--- lib/needlinc/shared-pages/notifications.dart
+++ lib/needlinc/shared-pages/notifications.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -23,14 +25,223 @@ class _NotificationsPageState extends State<NotificationsPage> {
           ),
         ),
       actions: [
-        IconButton(
-          icon: Icon(Icons.more_vert)
+        Container(
+          margin: EdgeInsets.only(right: 2),
+          child: IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
         ),
       ],
         elevation: 0.0,
 
       ),
-      body: Center(child: Text("No Notifications here"),),
+      body: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 11),
+            child: Text(
+                "Today",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+          ListTile(
+            title: Text("${Notifications[0][0]??"No messages found"}"),
+            trailing: Text(
+              "Nov",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              // Handle tap on Item 2
+            },
+          ),
+        ],
+      ),
     );
   }
 }
+
+List<List> Notifications = [[
+  "You Just set an appointment with JOHN DOE an electrician by 7:30 on Thursday",
+  "Hurrray! Enjoy a special discount of 30% on Iphone 11 for sale on Marketplace. Valid for 3 weeks."
+  ],
+  ["your post on Markertplace hs recerived 12 reactions.Check out the engagement",
+  "Congratulations! Your account has been successfully created.Start exploring NEEDLINC now!",
+  "Welcome to NEEDLINC! Get ready to explore and enjoy our app's exciting service and features"
+]];
