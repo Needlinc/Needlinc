65784b1aa9978d806492ffccf79e8cc6cee74398
XRichardroidX <oguzieibehrichard@gmail.com>
2023-12-20 22:58:20 +0100

    remove facebook auth plugin

--- lib/needlinc/backend/authentication/sign-up.dart
+++ lib/needlinc/backend/authentication/sign-up.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'dart:typed_data';
 import '../user-account/functionality.dart';
 import '../user-account/user-online-information.dart';
