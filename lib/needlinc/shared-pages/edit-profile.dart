c061821a58c89c3565ea8baa439d4248643e66b6
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-12-23 21:54:04 +0100

    complete edit-profile AppBar

--- lib/needlinc/shared-pages/edit-profile.dart
+++ lib/needlinc/shared-pages/edit-profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../colors/colors.dart';
+
 class editProfile extends StatefulWidget {
   const editProfile({super.key});
 
@@ -10,6 +12,31 @@ class editProfile extends StatefulWidget {
 class _editProfileState extends State<editProfile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: NeedlincColors.blue1),
+        backgroundColor: NeedlincColors.white,
+        leading: Icon(Icons.arrow_back_ios_new),
+        title: const Text(
+          'John Doe',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 15,
+          ),
+        ),
+        elevation: 0,
+        actions: [
+          InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              margin: const EdgeInsets.only(right: 10.0),
+              child: const Icon(Icons.more_vert),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
