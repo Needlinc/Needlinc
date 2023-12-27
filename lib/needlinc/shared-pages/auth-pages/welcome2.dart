fc6dae29888cfbbe9d4e15c698d01fa0745e3bb4
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-12-27 05:49:36 +0100

    Enable swipe left and right feature

--- lib/needlinc/shared-pages/auth-pages/welcome2.dart
+++ lib/needlinc/shared-pages/auth-pages/welcome2.dart
@@ -35,103 +35,125 @@ class _WelcomePage2State extends State<WelcomePage2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Stack(
-              // Tripple Layer
-              children: [
-                const backGround(),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.34,
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.only(left: 40, right: 40),
-                  alignment: Alignment.center,
-                  child: Text(
-                    showNext ? words.last : words.first,
-                    style: const TextStyle(fontSize: 20, color: NeedlincColors.white),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                const SizedBox(height: 135),
-              ],
-            ),
-            // NeedLinc image
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.37,
-              margin: const EdgeInsets.fromLTRB(0, 25, 0, 0),
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage("assets/1.png"),
-                ),
-              ),
-            ),
-            // Next button
-            Visibility(
-              visible: showNext,
-              child: Container(
-                alignment: Alignment.bottomRight,
-                margin: const EdgeInsets.only(right: 20, top: 60),
-                child: ElevatedButton(
-                  onPressed: () => Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => const SignupPage())),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    backgroundColor: NeedlincColors.blue1,
-                    padding: const EdgeInsets.all(16),
-                  ),
-                  child: const Text(
-                    "NEXT",
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            // Little circles below
-            Container(
-              padding: const EdgeInsets.all(30.0),
-              height:
-                  showNext ? null : MediaQuery.of(context).size.height * 0.37,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: NeedlincColors.white,
+      body: SafeArea(
+        child: GestureDetector(
+          onHorizontalDragEnd: (details) {
+            // Check if the user swiped left or right
+            if (details.primaryVelocity! > 0) {
+              _HideNext();
+            } else {
+              _ShowNext();
+            }
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Stack(
+                // Tripple Layer
                 children: [
-                  GestureDetector(
-                    onTap: _HideNext,
-                    child: Container(
-                      width: showNext ? small : big,
-                      height: showNext ? small : big,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                            0.5 * (showNext ? small : big)),
-                        color: showNext ? inactiveColor : activeColor,
+                  const backGround(),
+                  Container(
+                    height: 310,
+                    padding: const EdgeInsets.only(left: 40, right: 40),
+                    alignment: Alignment.center,
+                    child: RichText(
+                      textAlign: TextAlign.center,
+                      text: const TextSpan(
+                        text: 'NEEDLINC ',
+                        style: TextStyle(
+                            fontSize: 23, color: NeedlincColors.white),
+                        children: [
+                          TextSpan(
+                            text:
+                                'connects all FUTO students to freelancers or workers who are nearby',
+                            style: TextStyle(
+                                fontSize: 23,
+                                color: NeedlincColors.white,
+                                fontFamily: 'Comfortaa-Regular'),
+                          )
+                        ],
                       ),
                     ),
                   ),
-                  const SizedBox(width: 4.0),
-                  GestureDetector(
-                    onTap: _ShowNext,
-                    child: Container(
-                      width: showNext ? big : small,
-                      height: showNext ? big : small,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                            0.5 * (showNext ? big : small)),
-                        color: showNext ? activeColor : inactiveColor,
-                      ),
-                    ),
-                  ),
+                  const SizedBox(height: 135),
                 ],
               ),
-            ),
-          ],
+              // NeedLinc image
+              Container(
+                width: double.infinity,
+                height: 350,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage("assets/logo.png"),
+                  ),
+                ),
+              ),
+              // Next button
+              Visibility(
+                visible: showNext,
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  margin: const EdgeInsets.only(right: 20, top: 5),
+                  child: ElevatedButton(
+                    onPressed: () => Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignupPage())),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      backgroundColor: NeedlincColors.blue1,
+                      padding: const EdgeInsets.all(16),
+                    ),
+                    child: const Text(
+                      "NEXT",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // Little circles below
+              Container(
+                padding: const EdgeInsets.all(30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: _HideNext,
+                      child: Container(
+                        width: showNext ? small : big,
+                        height: showNext ? small : big,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                              0.5 * (showNext ? small : big)),
+                          color: showNext ? inactiveColor : activeColor,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 4.0),
+                    GestureDetector(
+                      onTap: _ShowNext,
+                      child: Container(
+                        width: showNext ? big : small,
+                        height: showNext ? big : small,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                              0.5 * (showNext ? big : small)),
+                          color: showNext ? activeColor : inactiveColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
