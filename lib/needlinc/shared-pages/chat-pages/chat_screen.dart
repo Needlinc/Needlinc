86948fbc4be7ba8780c33918e7205a39bceb2832
XRichardroidX <oguzieibehrichard@gmail.com>
2024-01-04 20:21:38 +0100

    connect chats to firebase

--- lib/needlinc/shared-pages/chat-pages/chat_screen.dart
+++ lib/needlinc/shared-pages/chat-pages/chat_screen.dart
@@ -95,16 +95,89 @@ class _ChatScreenState extends State<ChatScreen> {
                   return Text("User not found");
                 }
 
-                var userDocument = snapshot.data!;
+                var data = snapshot.data!.docs;
                 // Here, you can use `userDocument` to get user-specific data
                 // For demonstration, let's assume 'messages' is a field in the user document
                 // var messages = userDocument['messages'] as List<Message>;
 
                 // Dummy messages list (replace this with data from Firestore)
+                return ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    var chatData = data[index].data() as Map<String, dynamic>; // Access each document's data
 
-
-                return Container(
-
+                    // Now you can use userDocument to build your UI
+                    // For example, if you want to display a user's name and message:
+                    return Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10),
+                      padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 12.0),
+                      color: NeedlincColors.white,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  // Navigator.pushReplacement(context, MaterialPageRoute(
+                                  //   builder: (context) => ClientMainPages(currentPage: 4),
+                                  // ));
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.all(20),
+                                  margin: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        "profilePicture",
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                    color: NeedlincColors.black3,
+                                    shape: BoxShape.circle,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: MediaQuery
+                                    .of(context)
+                                    .size
+                                    .width * 0.75,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment
+                                      .start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment
+                                          .spaceBetween,
+                                      children: [
+                                        Text('${chatData['myUserName']}', style: const TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold),),
+                                      ],
+                                    ),
+                                    Text("userCategory", style: const TextStyle(
+                                        fontSize: 13,
+                                        fontWeight: FontWeight.w600)),
+                                    Text("📍address", style: const TextStyle(
+                                        fontSize: 12,
+                                        color: NeedlincColors.black2))
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 10, left: 65, bottom: 10),
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                                '${chatData['text']}',
+                                style: const TextStyle(fontSize: 18)
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 );
               },
             ),
